0d305c5cb1c0fe7ad61c787833674f29e8419efe
subbamaggus <markus.a.kerle@gmail.com>
2020-12-17 14:03:26 +0100

    write both variants to files

--- www/readdata.py
+++ www/readdata.py
@@ -9,24 +9,36 @@ import time
 import datetime
 
 from ADS1x15 import ADS1015
+from mq import *
 
-adc = ADS1015()
+try:
+    print('Reading ADS1x15 values, press Ctrl-C to quit...')
+    
+    adc = ADS1015()
+    mq = MQ();
+    
+    smbus_analog_0= open("smbus_A0.csv","a+")    
+    
+    while True:
+        date = datetime.datetime.now()
+    
+        value = adc.read_adc(0, 1)
+        perc = mq.MQPercentage()
+    
+        print('| {} | {} | {}'.format(value, date, int(perc["OZONE"])))
 
-GAIN = 1
+        smbus_analog_0.write('{},{},{}\n'.format(date, value, int(perc["OZONE"])))
+    
+        smbus_analog_0_curr= open("smbus_A0.current","w")
+        smbus_analog_0_curr.write('{}\n'.format(value))
+        smbus_analog_0_curr.close()
+        
 
-smbus_analog_0= open("smbus_A0.csv","a+")
+        smbus_analog_0_curr= open("smbus_A0.ppm","w")
+        smbus_analog_0_curr.write('{}\n'.format(int(perc["OZONE"])))
+        smbus_analog_0_curr.close()
+    
+        time.sleep(0.5)
 
-print('Reading ADS1x15 values, press Ctrl-C to quit...')
-
-while True:
-    value = adc.read_adc(0, gain=GAIN)
-    date = datetime.datetime.now()
-    print('| {} | {}'.format(value, date))
-
-    smbus_analog_0.write('{},{}\n'.format(date, value))
-
-    smbus_analog_0_curr= open("smbus_A0.current","w")
-    smbus_analog_0_curr.write('{}\n'.format(value))
-    smbus_analog_0_curr.close()
-
-    time.sleep(0.5)
+except:
+    print("\nAbort by user")
