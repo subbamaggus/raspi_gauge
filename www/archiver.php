4e5f18f70649f6470c2335d597be499c4155b4ed
subbamaggus <markus.a.kerle@gmail.com>
2021-03-21 13:14:11 +0100

    add authentication

--- www/archiver.php
+++ www/archiver.php
@@ -1,5 +1,14 @@
 <?php
 
+$auth = 'f03ada5ae38129d70e0b3c9992df812c';
+
+if ($_GET['auth'] != $auth) {
+    header('WWW-Authenticate: Basic realm="My Realm"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo 'not allowed';
+    exit;    
+}
+
 $data = json_decode(file_get_contents('php://input'), true);
 print_r($data);
 echo $data["data"];
