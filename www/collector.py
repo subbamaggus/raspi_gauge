d431c65741a09aeca2b2dd5e853d8d3d8b37f208
subbamaggus <markus.a.kerle@gmail.com>
2021-03-21 12:07:38 +0100

    first version of collector

---
+++ www/collector.py
@@ -0,0 +1,32 @@
+import json
+import requests
+import csv
+
+print('building data')
+
+data = {}
+
+with open('data/data.csv') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',')
+    for row in spamreader:
+        print(':'.join(row))
+    
+        data['data'] = []
+        data['data'].append({
+            'date': row[0],
+            'sensor': 'data',
+            'value': row[1]
+        })
+
+print('data: {}'.format(json.dumps(data)))
+
+print('building request')
+
+url = "http://localhost/archiver.php"
+#data = {'sender': 'Alice', 'receiver': 'Bob', 'message': 'We did it!'}
+headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
+print('sending request')
+r = requests.post(url, data=json.dumps(data), headers=headers)
+
+print('request status {}'.format(r.status_code))
