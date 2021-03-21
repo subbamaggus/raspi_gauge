dd2ae339ba9a37c2a620dd1f5cd58345a6255c37
subbamaggus <markus.a.kerle@gmail.com>
2021-03-21 13:05:05 +0100

    strip data for easier use

--- www/collector.py
+++ www/collector.py
@@ -3,7 +3,6 @@ import requests
 import csv
 
 print('building data')
-
 data = {}
 
 with open('data/data.csv') as csvfile:
@@ -13,20 +12,20 @@ with open('data/data.csv') as csvfile:
     
         data['data'] = ({
             'date': row[0],
-            'sensor': 'data',
             'value': row[1]
         })
 
 print('json: {}'.format(json.dumps(data)))
 
-print('building request')
 
+print('building request')
 url = "http://localhost/archiver.php"
-#data = {'sender': 'Alice', 'receiver': 'Bob', 'message': 'We did it!'}
 headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
 
 print('sending request')
 r = requests.post(url, data=json.dumps(data), headers=headers)
+
 
 print('request status {}'.format(r.status_code))
 print(r.content)
