aa39a9fe56167eafa77808b1652bd82bc15a0948
subbamaggus <markus.a.kerle@gmail.com>
2021-03-21 11:33:05 +0100

    just write one data file with csv

--- www/readdata.py
+++ www/readdata.py
@@ -20,8 +20,6 @@ try:
     adc = ADS1115()
     mq = MQ();
     
-    smbus_analog_0= open("smbus_A0.csv","a+")    
-    
     while True:
         date = datetime.datetime.now()
     
@@ -29,25 +27,12 @@ try:
         perc = mq.MQPercentage()
         humidity, temperature = Adafruit_DHT.read_retry(11, 4)
     
-        print('| {} | {} | {} | {} | {} | {}'.format(date, value, value/4, humidity, temperature, perc["OZONE"]))
+        print('| {} | {} | {} | {} | {}'.format(date, value, humidity, temperature, perc["OZONE"]))
 
-        smbus_analog_0.write('{},{},{},{},{},{}\n'.format(date, value, value/4, humidity, temperature, int(perc["OZONE"])))
-    
-        smbus_analog_0_curr= open("smbus_A0.current","w")
-        smbus_analog_0_curr.write('{}\n'.format(value))
+        smbus_analog_0_curr= open("data.csv","w")
+        smbus_analog_0_curr.write('{},{},{},{},{}\n'.format(date, value, humidity, temperature, int(perc["OZONE"])))
         smbus_analog_0_curr.close()
         
-
-        smbus_analog_0_curr= open("smbus_A0.ppb","w")
-        smbus_analog_0_curr.write('{}\n'.format(int(perc["OZONE"] * 1000)))
-        smbus_analog_0_curr.close()
-    
-        # ppm to ugpm3 >> 200ug/m3 ~ 0.1 ppm
-        # this sensor is from 2PPB to 10PPM >> from 0.4 ug/m3 to 20000ug/m3
-        smbus_analog_0_curr= open("smbus_A0.ugpm3","w")
-        smbus_analog_0_curr.write('{}\n'.format(int(perc["OZONE"] * 2000)))
-        smbus_analog_0_curr.close()
-    
         time.sleep(0.5)
 
 except:
